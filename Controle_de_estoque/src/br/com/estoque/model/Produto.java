794068a9cb67be2ee828e1313fbcbecd546f3a28
diego.correa <diego.correa@RJDSKDEV621.medilab.local>
2016-09-26 14:50:11 -0300

    Ajuste funcional

---
+++ Controle_de_estoque/src/br/com/estoque/model/Produto.java
@@ -0,0 +1,46 @@
+package br.com.estoque.model;
+
+public class Produto {
+	
+	private Integer id;
+	private String nome;
+	private Double preco;
+	private String fornecedor;
+	public Produto(Integer id, String nome, Double preco, String fornecedor) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.fornecedor = fornecedor;
+	}
+	public Produto() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(String fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	
+	
+
+}
---
+++ Controle_de_estoque/src/br/com/estoque/persistence/HibernateUtil.java
@@ -0,0 +1,40 @@
+package br.com.estoque.persistence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			if (sessionFactory == null) {
+				Configuration configuration = new Configuration()
+						.configure(HibernateUtil.class
+								.getResource("/br.com.estoque.config/hibernate.cfg.xml"));
+				ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder();
+				serviceRegistryBuilder.applySettings(configuration
+						.getProperties());
+				ServiceRegistry serviceRegistry = serviceRegistryBuilder.buildServiceRegistry();
+				sessionFactory = configuration
+						.buildSessionFactory(serviceRegistry);
+			}
+			return sessionFactory;
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
+}
+
