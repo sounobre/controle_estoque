f7948af1ed49f03c185558514f086644e7869ff7
sounobre <sounobre@hotmail.com>
2016-10-18 16:50:51 -0200

    jquery on

---
+++ Controle_de_estoque/WebContent/resources/js/js.js
@@ -0,0 +1,13 @@
+
+	$(document).ready(function(){
+	    $("#hide").click(function(){
+	        $("#teste").hide();
+	    });
+	    $("#hide").click(function(){
+	        $("#teste1").hide();
+	    });
+	    $("#show").click(function(){
+	        $("#teste").show();
+	    });
+	});
+
